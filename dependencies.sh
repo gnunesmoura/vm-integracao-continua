729b2b5a2bd220bf018a2f55aae3f8d66efc240a
Gustavo Moura <gnunes.moura@gmail.com>
2019-09-22 14:42:21 -0300

    correção do script de dependencias

--- dependencies.sh
+++ dependencies.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y --install-recommends --force linux-generic-lts-xenial 
-sudo apt-get upgrade -y
+sudo apt-get -q update 
+sudo apt-get -q upgrade -y
+sudo apt-get -q install -y --install-recommends linux-generic-lts-xenial 
 sudo sysctl -w vm.max_map_count=262144
 sudo sysctl -w fs.file-max=65536
-sudo apt-get install -y git unzip
+sudo apt-get -q install git unzip -y
 
-wget  -P ./ "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip" >> /dev/null
-unzip sonar-scanner-cli-4.0.0.1744-linux.zip  >> /dev/null
+wget -q -P ./ "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip"
+unzip -q sonar-scanner-cli-4.0.0.1744-linux.zip
 echo 'export PATH="/home/vagrant/sonar-scanner-4.0.0.1744-linux/bin:$PATH"' >> ~/.bashrc 
+
+sudo reboot

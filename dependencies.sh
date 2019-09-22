259dd6a327daa19ae43648bbf3226f0d27946dec
Gustavo Moura <gnunes.moura@gmail.com>
2019-09-22 13:04:07 -0300

    voltando a ordem antiga

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get upgrade -y
 sudo apt-get install -y --install-recommends --force linux-generic-lts-xenial 
+sudo apt-get upgrade -y
 sudo sysctl -w vm.max_map_count=262144
 sudo sysctl -w fs.file-max=65536
 sudo apt-get install -y git unzip
